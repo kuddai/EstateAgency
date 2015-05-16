bdbf15324724c3e18f20c0e71a7befadddd447e8
kuddai <kuddai92@yandex.ru>
2015-05-16 13:46:28 +0300

    first version of business card is ready

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('EstateAgency', [
   'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider
+  /*$routeProvider
       .when('/view1', {
         templateUrl: 'view1/view1.html',
         controller: 'View1Ctrl'
       })
-      .otherwise({redirectTo: '/view1'});
+      .otherwise({redirectTo: '/view1'});*/
 }]);
---
+++ app/js/directives/businessCard.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kuddai on 16.05.2015.
+ */
+app.directive('businessCard', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/directives/businessCard.html'
+    };
+});
